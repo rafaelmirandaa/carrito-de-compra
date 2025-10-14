127b637ed9b3d5c283199a4a9b3a04c401718581
rafaelmirandaa <rafael.miranda.almendaris@gmail.com>
2025-10-13 23:52:34 -0500

    se pudo pero vamos avanzando

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
+ let productos = [];
+
 function iniciarApp(){
-
     obtenerDatos();
-
+}
     function obtenerDatos(){
-        const datos = "/data.json";
-        fetch(datos)
+        const rutas = "/data.json";
+        fetch(rutas)
             .then((respuesta)=> respuesta.json())
             .then((datos) =>{
-                console.log(datos);
+                productos = datos;
+                llenarCards();
             })
             .catch((error) => {
                 console.error("No me puedo conectar");
             });
     }
-    function leerDatosPlatos(platos){
-        const infoplatos = {
-            imagen:platos.querryselector('img').src,
-            name:platos.querryselector('h2').textContent
-        }
-            console.log(infoplatos);
-    }
 
+    function llenarCards(){
+        const cards = document.querySelectorAll('.producto'); // tomamos el producto correspondiente
+            
+            cards.forEach((card, index) => {
+                const producto = productos[index]; // tomamos el producto correspondiente
+                 if (!producto) return;
 
-}    
+            // Imágenes
+            card.querySelector('.img-desktop').srcset = producto.image.desktop;
+            card.querySelector('.img-tablet').srcset = producto.image.tablet;
+            card.querySelector('.img-mobile').src = producto.image.mobile;
+            card.querySelector('.img-mobile').alt = producto.name;
+
+            // Textos
+            card.querySelector('.category').textContent = producto.category;
+            card.querySelector('.name').textContent = producto.name;
+            card.querySelector('.price').textContent = `$${producto.price}`;
+    });
+}        
 document.addEventListener('DOMContentLoaded', iniciarApp);
